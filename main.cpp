a0eef365ec93bbc071ee9537e6bdcef36dcd81f2
Max Lu <luyiren@seas.upenn.edu>
2017-04-10 20:00:30 -0400

    clean up tests

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 #include <assert.h>
 
 void test_conv_layer() {
+  std::cout<< "Testing Conv2D .."<<std::endl;
   // inputs: filter_height, filter_width, in_channels, out_channels, stride
   Conv2D<float>* conv_layer = new Conv2D<float>(5, 5, 3, 5, 1);
-  // std::cout<< conv_layer->kernel_width << " " << conv_layer->kernel_height <<std::endl;
   assert(conv_layer->kernel_height==5);
   assert(conv_layer->kernel_width==5);
   assert(conv_layer->in_channels==3);
@@ -16,15 +16,12 @@ void test_conv_layer() {
 }
 
 void test_tensor() {
+  std::cout<< "Testing Tensor .."<<std::endl;
   // inputs: tensor dimensions
   Tensor<float>* tensor = new Tensor<float>({3,3,3});
-  // std::cout<< tensor->GetIdx({2,2,2}) <<std::endl;
   assert(tensor->GetIdx({2,2,2})==26);
-  // std::cout<< tensor->GetIdx({1,2,2}) <<std::endl;
   assert(tensor->GetIdx({1,2,2})==17);
-  // std::cout<< tensor->GetIdx({2,1,2}) <<std::endl;
   assert(tensor->GetIdx({2,1,2})==23);
-  // std::cout<< tensor->GetIdx({2,2,1}) <<std::endl;
   assert(tensor->GetIdx({2,2,1})==25);
   delete tensor;
 }
