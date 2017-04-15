8270505a98d9314a1c9a96906a03d51b20dfd5fc
Max Lu <luyiren@seas.upenn.edu>
2017-04-15 17:06:31 -0400

    update tests

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "basics/session.hpp"
 #include "initializers/gaussian_kernel_initializer.hpp"
 #include <assert.h>
+#include <cmath>
 
 void test_session() {
   std::cout<< "Testing Session .."<<std::endl;
@@ -38,18 +39,18 @@ void test_tensor() {
 void test_gaussian_kernel() {
   std::cout<< "Testing gaussian kernel initializer .. "<<std::endl;  
   std::cout<<Session::GetNewSession()->gpu<<std::endl;
-  Tensor<float>* W = new Tensor<float>({5,5,1,1});
-  Tensor<float>* b = new Tensor<float>({1});
-  GaussianKernelInitializer<float>(5.0).Initialize(W, b);
-  for (unsigned i = 0; i < W->GetDims()[0]; i++) {
-    for (unsigned j = 0; j < W->GetDims()[1]; j++) {
-      std::cout<<W->at({i, j, 0, 0})<<"\t";
+  Tensor<float>W = Tensor<float>({5,5,1,1});
+  Tensor<float>b = Tensor<float>({1});
+  GaussianKernelInitializer<float>(5.0).Initialize(&W, &b);
+  double sum = 0.0;
+  for (unsigned i = 0; i < W.GetDims()[0]; i++) {
+    for (unsigned j = 0; j < W.GetDims()[1]; j++) {
+      sum += W.at({i, j, 0, 0});
+      std::cout<<W.at({i, j, 0, 0})<<"\t";
     }
     std::cout<<std::endl;
   }
-
-  delete W;
-  delete b;
+  assert(std::abs(sum-1.0)<0.00001);
 }
 
 
