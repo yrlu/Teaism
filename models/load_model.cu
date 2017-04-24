d0074fdf4a6d6f87cb8c30ebe2450c53b1359f36
jyhjinghwang <jyhjinghwang@gmail.com>
2017-04-23 19:13:19 -0700

    load model

--- models/load_model.cu
+++ models/load_model.cu
@@ -6,23 +6,54 @@
 #include "basics/tensor.cu"
 #include "layers/conv2d.cu"
 #include "layers/fc.cu"
+#include "utils/load_model.hpp"
 
+void show_tensor(Tensor<float>* t) {
+  const size_t* dims = t->GetDims();
+  for (int o = 0; o < dims[3]; ++o) {
+    for (int i = 0; i < dims[2]; ++i) {
+      for (int w = 0; w < dims[1]; ++w) {
+        for (int h = 0; h < dims[0]; ++h) {
+          printf("(%d, %d, %d, %d): %f\n", h, w, i, o, t->at(h,w,i,o));
+        }
+      }
+    }
+  }
+}
+
+void show_fc_data(Tensor<float>* t) {
+  const size_t* dims = t->GetDims();
+  for (int o = 0; o < dims[2]; ++o) {
+    for (int i = 0; i < dims[3]; ++i) {
+      printf("(%d, %d, %d, %d): %f\n", 0,0,o,i, t->at(0,0,o,i));
+    }
+  }
+}
 
 void test_load_model() {
 
   std::string model_path = "models/alexnet/model.txt";
 
   std::ifstream file(model_path);
-  std::string tmp_string;
-  float tmp_float;
-  while(std::getline(file, tmp_string)) {
-//    std::istringstream iss(tmp);
-//    iss >> tmp_string;
-    std::cout << tmp_string << std::endl;
-    tmp_float = std::stof(tmp_string);
-    std::cout << tmp_float << std::endl;
-  }
+  
+  // conv1_w
+  size_t conv1_w_dims[4] = {11,11,3,96};
+  Tensor<float>* conv1_w = Tensor<float>::CreateTensorCPU(conv1_w_dims);
+//  load_to_conv<float>(conv1_w, file);
 
+  // fc8_w
+  size_t fc8_w_dims[4] = {1,1,1000,4096};
+  Tensor<float>* fc8_w = Tensor<float>::CreateTensorCPU(fc8_w_dims);
+  load_to_fc(fc8_w, file);
+
+  // fc8_b
+  size_t fc8_b_dims[4] = {1,1,1,1000};
+  Tensor<float>* fc8_b = Tensor<float>::CreateTensorCPU(fc8_b_dims);
+  load_to_bias(fc8_b, file);
+
+//  show_tensor(conv1_w);
+//  show_fc_data(fc8_w);
+//  show_tensor(fc8_b);
 }
 
 
---
+++ utils/load_model.hpp
@@ -0,0 +1,68 @@
+#ifndef LOAD_MODEL_HPP_
+#define LOAD_MODEL_HPP_
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include "basics/tensor.cu"
+
+template <class Dtype>
+void load_to_conv(Tensor<Dtype>* t, std::ifstream &file) {
+  std::string tmp_string;
+  std::string tmp_token;
+  Dtype tmp;
+  const size_t* dims = t->GetDims();
+
+  printf("Loading conv: (%d, %d, %d, %d): \n", dims[0],dims[1],dims[2],dims[3]);
+  std::getline(file, tmp_string);
+  std::istringstream iss(tmp_string);
+  for (int o = 0; o < dims[3]; ++o) {
+    for (int i = 0; i < dims[2]; ++i) {
+      for (int w = 0; w < dims[1]; ++w) {
+        for (int h = 0; h < dims[0]; ++h) {
+          iss >> tmp_token;
+          tmp = std::stof(tmp_token);
+          t->at(h,w,i,o) = tmp;
+        }
+      }
+    }
+  }
+}
+
+template <class Dtype>
+void load_to_fc(Tensor<Dtype>* t, std::ifstream &file) {
+  std::string tmp_string;
+  std::string tmp_token;
+  Dtype tmp;
+  const size_t* dims = t->GetDims();
+
+  printf("Loading fc: (%d, %d, %d, %d): \n", 1,1,dims[2],dims[3]);
+  std::getline(file, tmp_string);
+  std::istringstream iss(tmp_string);
+  for (int o = 0; o < dims[2]; ++o) {
+    for (int i = 0; i < dims[3]; ++i) {
+      iss >> tmp_token;
+      tmp = std::stof(tmp_token);
+      t->at(0,0,o,i) = tmp;
+    }
+  }
+}
+
+template <class Dtype>
+void load_to_bias(Tensor<Dtype>* t, std::ifstream &file) {
+  std::string tmp_string;
+  std::string tmp_token;
+  Dtype tmp;
+  const size_t* dims = t->GetDims();
+
+  printf("Loading bias: (%d, %d, %d, %d): ", 1,1,1,dims[3]);
+  std::getline(file, tmp_string);
+  std::istringstream iss(tmp_string);
+  for (int o = 0; o < dims[3]; ++o) {
+    iss >> tmp_token;
+    tmp = std::stof(tmp_token);
+    t->at(0,0,0,o) = tmp;
+  }
+}
+
+#endif  // LOAD_MODEL_HPP_
