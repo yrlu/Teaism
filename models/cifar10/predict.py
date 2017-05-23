da944bc8a4b527901ffd457ff98c03a1eaa8f2b5
jyhjinghwang <jyhjinghwang@gmail.com>
2017-05-22 21:25:45 -0700

    show caffe gradient

--- models/cifar10/predict.py
+++ models/cifar10/predict.py
@@ -13,13 +13,21 @@ for i in range(1):
   img -= [128, 128, 128]
   img = img[:,:,::-1]
   img = img.transpose([2,0,1])
+  label = np.empty([1,1,1,1])
+  label[0] = 6
 
   net.blobs['data'].data[...] = img
+  net.blobs['label'].data[...] = label
 
   net.forward()
+  net.backward()
 
-  print net.blobs['prob'].data[...].tolist()
-  print net.blobs['prob'].data.shape
+#  print net.blobs['prob'].data[...].tolist()
+#  print net.blobs['prob'].data.shape
 #  print net.params['conv1'][0].data
 #  print net.params['conv1'][0].data.shape
+  print net.blobs['fc5'].diff[...].tolist()
+  print net.blobs['fc5'].diff.shape
+  print net.params['fc5'][0].diff
+  print net.params['fc5'][0].diff.shape
 
